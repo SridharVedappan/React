5e25d84968e8d3e7edd42da8584715096ac55aee
SridharVedappan <sridharvedappan@thestackly.com>
2026-04-09 18:19:28 +0530

    Task 6 completed

---
+++ task-6/stopWatch/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Stopwatch from "./StopWatch";
+
+function App() {
+  return (
+    <>
+      <Stopwatch />
+    </>
+  );
+}
+
+export default App;
---
+++ task-6/stopWatch/src/StopWatch.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+export default function Stopwatch() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const formatTime = () => {
+    const hours = String(Math.floor(time / 3600)).padStart(2, "0");
+    const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, "0");
+    const seconds = String(time % 60).padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  const handleStart = () => setIsRunning(true);
+  const handleStop = () => setIsRunning(false);
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  return (
+    <div className="container">
+      <div className="stopwatch-card">
+        <h1>Stopwatch</h1>
+        <h2 className="time">{formatTime()}</h2>
+
+        <div className="buttons">
+          <button onClick={handleStart}>Start</button>
+          <button onClick={handleStop}>Stop</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      </div>
+    </div>
+  );
+}
